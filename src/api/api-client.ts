3e61e7724452c57e32331c2d924c08e2bec2eb56
Shoetan <emmysoetan@gmail.com>
2024-11-01 14:17:26 +0100

    cors error

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -6,7 +6,7 @@ const api = axios.create({
   baseURL: baseURL,
   headers :{
     "Content-Type":"application/json",
-    "Referrer-Policy": "strict-origin-when-cross-origin"
+    "Referrer-Policy": "strict-origin-when-cross-origin",
   }
 })
 
